d53db71b720b882cd8a227598c96eb20c9f541ef
Nicolas Diz <diz.nicolasamor@gmail.com>
2024-01-07 10:08:21 -0300

    MASTER: fix eslint error

--- src/index.js
+++ src/index.js
@@ -3,5 +3,6 @@ import ReactDOM from 'react-dom';
 import ProfileContainer from './profile/ProfileContainer';
 import registerServiceWorker from './registerServiceWorker';
 
+// eslint-disable-next-line react/no-deprecated
 ReactDOM.render(<ProfileContainer />, document.getElementById('root'));
 registerServiceWorker();
